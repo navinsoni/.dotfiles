e4bcba3828761a290f998b6f2a0b239831bdd88b
Navin Soni <navinns@amazon.com>
2021-08-31 14:10:16 -0700

    Add copy to clipboard

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -9,3 +9,5 @@ let test#strategy = "neovim"
 let test#custom_runners = {'java': ['BrazilTest']}
 let test#java#runner = 'braziltest'
 let test#enabled_runners = ["java#braziltest"]
+
+set clipboard+=unnamedplus
--- nvim/.config/nvim/lua/nv/lspsaga-config.lua
+++ nvim/.config/nvim/lua/nv/lspsaga-config.lua
@@ -1,13 +1,16 @@
 vim.api.nvim_set_keymap('n', '<Leader>rr', ':Lspsaga rename<CR>', { noremap = true, silent = true})
 
 
+vim.lsp.handlers["textDocument/references"] = require("telescope.builtin").lsp_references
 vim.api.nvim_set_keymap('n', 'K', ':Lspsaga hover_doc<CR>', {noremap = true, silent = true})
 vim.cmd("nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>")
 vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
 vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
 vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.cmd("nnoremap <silent> f <cmd>lua vim.lsp.buf.formatting()<CR>")
+
 vim.cmd("nnoremap <silent> ca :Lspsaga code_action<CR>")
--- vim.cmd("nnoremap <silent> K :Lspsaga hover_doc<CR>")
+vim.cmd("nnoremap <silent> K :Lspsaga hover_doc<CR>")
 -- vim.cmd('nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>')
 vim.cmd("nnoremap <silent> <C-p> :Lspsaga diagnostic_jump_prev<CR>")
 vim.cmd("nnoremap <silent> <C-n> :Lspsaga diagnostic_jump_next<CR>")
